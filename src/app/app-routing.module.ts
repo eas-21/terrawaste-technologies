cddd94e0a30a4e3e609e94affdfecd201a1894d8
eas-21 <easwariswathi@gmail.com>
2024-07-10 12:34:45 +0530

    twt code pushed

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+import { HomeComponent } from './core/home/home.component';
+import { ServiceComponent } from './core/service/service.component';
+import { IOTComponent } from './core/service/iot/iot.component';
+import { AboutComponent } from './core/about/about.component';
+import { ProjectComponent } from './core/project/project.component';
+import { ContactComponent } from './core/contact/contact.component';
+const routes: Routes = [{
+  path:'', component: AdminLayoutComponent,
+  children:[
+    {
+      path:'', component:HomeComponent,
+      loadChildren:()=>import('../app/core/home/home.module').then(m=>m.HomeModule)
+    },
+    {
+      path:'service', component:ServiceComponent,
+      loadChildren:()=>import('../app/core/service/service.module').then(m=>m.ServiceModule)
+    },
+    { path:'IOT', component: IOTComponent},
+    { path:'about', component: AboutComponent},
+    { path:'contact', component: ContactComponent},
+    { path:'project', component: ProjectComponent}
+  ]
+},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+import { ScrollTopComponent } from './shared/scroll-top/scroll-top.component';
+import { HomeComponent } from './core/home/home.component';
+import { AboutComponent } from './core/about/about.component';
+import { ContactComponent } from './core/contact/contact.component';
+import { ProjectComponent } from './core/project/project.component';
+import { ServiceComponent } from './core/service/service.component';
+import { IOTComponent } from './core/service/iot/iot.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent,
+    ScrollTopComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProjectComponent,
+    ServiceComponent,
+    IOTComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
